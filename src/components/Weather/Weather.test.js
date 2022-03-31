9abbdf34f91ab9edccd6f804b102b773dc6faf1a
Derek Eduardo Gonçalves <drkedug@gmail.com>
2022-03-31 17:26:00 -0300

    remove unused tests

--- src/components/Weather/Weather.test.js
+++ src/components/Weather/Weather.test.js
@@ -2,27 +2,9 @@ import { render, screen } from '@testing-library/react';
 import { Weather } from './Weather';
 
 describe('Test all labels', () => {
-  it('should render street label', () => {
+  it('should render Submit button', () => {
     render(<Weather />);
-    const streetElement = screen.getByText(/Structure and Street/i);
-    expect(streetElement).toBeInTheDocument();
-  });
-
-  it('should render City label', () => {
-    render(<Weather />);
-    const streetElement = screen.getByText(/City/i);
-    expect(streetElement).toBeInTheDocument();
-  });
-
-  it('should render State label', () => {
-    render(<Weather />);
-    const streetElement = screen.getByText(/State/i);
-    expect(streetElement).toBeInTheDocument();
-  });
-
-  it('should render ZIP code label', () => {
-    render(<Weather />);
-    const streetElement = screen.getByText(/ZIP Code/i);
-    expect(streetElement).toBeInTheDocument();
+    const SubmitButton = screen.getByText(/Submit/i);
+    expect(SubmitButton).toBeInTheDocument();
   });
 });
